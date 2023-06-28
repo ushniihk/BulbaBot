66170d51dde182b66971bde0444887ef1d93393e
ushnii <ushniihk@yandex.ru>
2023-06-28 22:37:56 +0300

    add help handler

--- ApplicationModule/src/main/java/com/belka/BulbaBot/handler/StartHandler.java
+++ ApplicationModule/src/main/java/com/belka/BulbaBot/handler/StartHandler.java
@@ -1,5 +1,7 @@
 package com.belka.BulbaBot.handler;
 
+import com.belka.BulbaBot.model.User;
+import com.belka.BulbaBot.repository.UserRepository;
 import com.belka.core.handlers.BelkaEvent;
 import com.belka.core.handlers.BelkaHandler;
 import com.belka.core.previous_step.PreviousService;
@@ -9,6 +11,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
+
+import java.sql.Timestamp;
 
 @Component
 @AllArgsConstructor
@@ -16,6 +22,8 @@ public class StartHandler implements BelkaHandler {
 
     private final static String CODE = "/start";
     private final PreviousService previousService;
+    private final UserRepository userRepository;
+
     @Override
     public PartialBotApiMethod<?> handle(BelkaEvent event) {
         if (event.isHasText() && event.getText().equals(CODE)) {
@@ -24,9 +32,24 @@ public class StartHandler implements BelkaHandler {
                     .previousStep(CODE)
                     .userId(chatId)
                     .build());
+            registerUser(event.getUpdate().getMessage());
             return startCommandReceived(chatId, event.getUpdate().getMessage().getChat().getFirstName());
         }
         return null;
+    }
+
+    private void registerUser(Message message) {
+        if (!userRepository.existsById(message.getChatId())) {
+            Chat chat = message.getChat();
+            User user = User.builder()
+                    .id(chat.getId())
+                    .firstname(chat.getFirstName())
+                    .lastname(chat.getLastName())
+                    .username(chat.getUserName())
+                    .registeredAt(new Timestamp(System.currentTimeMillis()))
+                    .build();
+            userRepository.save(user);
+        }
     }
 
     private SendMessage startCommandReceived(Long chatId, String name) {
