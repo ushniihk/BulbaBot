20e0c516178bb742fb1259e931e3fc1b656b507c
ushnii <ushniihk@yandex.ru>
2023-07-03 00:09:19 +0300

    add calendar

--- newDiary/src/main/java/com/belka/newDiary/handler/DiaryCalendarHandler.java
+++ newDiary/src/main/java/com/belka/newDiary/handler/DiaryCalendarHandler.java
@@ -4,18 +4,13 @@ import com.belka.core.handlers.BelkaEvent;
 import com.belka.core.handlers.BelkaHandler;
 import com.belka.core.previous_step.PreviousService;
 import com.belka.core.previous_step.dto.PreviousStepDto;
+import com.belka.newDiary.service.CalendarService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import reactor.core.publisher.Flux;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-import java.util.Locale;
+import java.time.LocalDate;
 
 import static com.belka.newDiary.handler.DiaryBaseHandler.GET_DIARY;
 
@@ -24,81 +19,38 @@ import static com.belka.newDiary.handler.DiaryBaseHandler.GET_DIARY;
 public class DiaryCalendarHandler implements BelkaHandler {
 
     private final static String CODE = "READ_DIARY";
-    private final static String CALENDAR = "Календарь";
+    private final static String PREVIOUS = "PREV-MONTH";
+    private final static String NEXT = "NEXT-MONTH";
     private final PreviousService previousService;
+    private final CalendarService calendarService;
 
 
     @Override
     public Flux<PartialBotApiMethod<?>> handle(BelkaEvent event) {
         if (event.getUpdate().hasCallbackQuery() && event.getData().equals(GET_DIARY)) {
             Long chatId = event.getChatId();
+            LocalDate date = LocalDate.now();
+            Integer YEAR = date.getYear();
+            Integer MONTH = date.getMonth().getValue()-1;
             previousService.save(PreviousStepDto.builder()
                     .previousStep(CODE)
                     .userId(chatId)
                     .build());
-            return Flux.just(sendCalendarMessage(chatId));
+            return Flux.just(calendarService.sendCalendarMessage(chatId, YEAR, MONTH));
+        } else if (event.getUpdate().hasCallbackQuery() && (event.getData().startsWith(PREVIOUS) || event.getData().startsWith(NEXT))) {
+            String dateString = event.getData().substring(11);
+            String[] dateArray = dateString.split("-");
+            Integer YEAR = Integer.parseInt(dateArray[0]);
+            Integer MONTH = Integer.parseInt(dateArray[1]);
+            Long chatId = event.getChatId();
+            previousService.save(PreviousStepDto.builder()
+                    .previousStep(CODE)
+                    .userId(chatId)
+                    .build());
+            return Flux.just(calendarService.sendCalendarMessage(chatId, YEAR, MONTH));
         }
+
         return null;
     }
 
-    public SendMessage sendCalendarMessage(long chatId) {
-        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
-        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
-        rows.add(addHeadersToCalendar());
-        rows.add(addDaysOfTheWeekToCalendar());
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.MONTH, 1);
-        for (int i = 1; i <= 5; i++) {
-            List<InlineKeyboardButton> row = new ArrayList<>();
-            rows.add(addDaysToCalendar(row, calendar));
-        }
-        markup.setKeyboard(rows);
-        SendMessage message = new SendMessage(String.valueOf(chatId), CALENDAR);
-        message.setReplyMarkup(markup);
-        return message;
-    }
-
-    private List<InlineKeyboardButton> addHeadersToCalendar() {
-        Calendar calendar = Calendar.getInstance();
-        List<InlineKeyboardButton> row = new ArrayList<>();
-        row.add(InlineKeyboardButton.builder()
-                .text("<<")
-                .callbackData("PREV-MONTH")
-                .build());
-        row.add(InlineKeyboardButton.builder()
-                .text(calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US) + " " + calendar.get(Calendar.YEAR))
-                .callbackData("IGNORE")
-                .build());
-        row.add(InlineKeyboardButton.builder()
-                .text(">>")
-                .callbackData("NEXT-MONTH")
-                .build());
-        return row;
-    }
-
-    private List<InlineKeyboardButton> addDaysOfTheWeekToCalendar() {
-        Calendar calendar = Calendar.getInstance();
-        List<InlineKeyboardButton> row = new ArrayList<>();
-        for (int i = 1; i <= 7; i++) {
-            InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
-            inlineKeyboardButton.setText(calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US));
-            inlineKeyboardButton.setCallbackData("DAY-" + calendar.get(Calendar.DAY_OF_MONTH));
-            row.add(inlineKeyboardButton);
-            calendar.add(Calendar.DAY_OF_MONTH, 1);
-        }
-        return row;
-    }
-
-    private List<InlineKeyboardButton> addDaysToCalendar(List<InlineKeyboardButton> row, Calendar calendar) {
-            for (int j = 1; j <= 7; j++) {
-                InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
-                inlineKeyboardButton.setText(String.valueOf(calendar.get(Calendar.DAY_OF_MONTH)));
-                inlineKeyboardButton.setCallbackData("DAY-" + calendar.get(Calendar.YEAR)
-                        + "." + calendar.get(Calendar.MONTH)
-                        + "." + calendar.get(Calendar.DAY_OF_MONTH));
-                row.add(inlineKeyboardButton);
-                calendar.add(Calendar.DAY_OF_MONTH, 1);
-            }
-        return row;
-    }
 }
---
+++ newDiary/src/main/java/com/belka/newDiary/service/CalendarService.java
@@ -0,0 +1,81 @@
+package com.belka.newDiary.service;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+@Component
+public class CalendarService {
+    private final static String CALENDAR = "Calendar";
+
+    public SendMessage sendCalendarMessage(long chatId, Integer year, Integer month) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, 1);
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+        rows.add(addHeadersToCalendar(calendar, year, month));
+        rows.add(addDaysOfTheWeekToCalendar(calendar));
+        calendar = Calendar.getInstance();
+        calendar.set(year, month, 1);
+        calendar.add(Calendar.MONTH, 1);
+        for (int i = 1; i <= 5; i++) {
+            List<InlineKeyboardButton> row = new ArrayList<>();
+            rows.add(addDaysToCalendar(row, calendar));
+        }
+        markup.setKeyboard(rows);
+        SendMessage message = new SendMessage(String.valueOf(chatId), CALENDAR);
+        message.setReplyMarkup(markup);
+        return message;
+    }
+
+    private List<InlineKeyboardButton> addHeadersToCalendar(Calendar calendar, Integer year, Integer month) {
+        List<InlineKeyboardButton> row = new ArrayList<>();
+/*        LocalDate date = LocalDate.calendar.getTime();
+        Integer YEAR = date.getYear();
+        Integer MONTH = date.getMonth().getValue();*/
+        row.add(InlineKeyboardButton.builder()
+                .text("<<")
+                .callbackData("PREV-MONTH." + year + "-" + (month - 1))
+                .build());
+        row.add(InlineKeyboardButton.builder()
+                .text(calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US) + " " + calendar.get(Calendar.YEAR))
+                .callbackData("IGNORE")
+                .build());
+        row.add(InlineKeyboardButton.builder()
+                .text(">>")
+                .callbackData("NEXT-MONTH" + year + "-" + (month + 1))
+                .build());
+        return row;
+    }
+
+    private List<InlineKeyboardButton> addDaysOfTheWeekToCalendar(Calendar calendar) {
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        for (int i = 1; i <= 7; i++) {
+            InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
+            inlineKeyboardButton.setText(calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US));
+            inlineKeyboardButton.setCallbackData("DAY-" + calendar.get(Calendar.DAY_OF_MONTH));
+            row.add(inlineKeyboardButton);
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+        return row;
+    }
+
+    private List<InlineKeyboardButton> addDaysToCalendar(List<InlineKeyboardButton> row, Calendar calendar) {
+        for (int j = 1; j <= 7; j++) {
+            InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
+            inlineKeyboardButton.setText(String.valueOf(calendar.get(Calendar.DAY_OF_MONTH)));
+            inlineKeyboardButton.setCallbackData("DAY-" + calendar.get(Calendar.YEAR)
+                    + "." + calendar.get(Calendar.MONTH)
+                    + "." + calendar.get(Calendar.DAY_OF_MONTH));
+            row.add(inlineKeyboardButton);
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+        return row;
+    }
+}
