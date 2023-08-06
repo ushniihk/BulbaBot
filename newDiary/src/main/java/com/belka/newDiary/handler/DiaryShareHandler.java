ccad4c308f1bd78e17eee33bfd6c0dd2f579c623
ushnii <ushniihk@yandex.ru>
2023-08-06 17:48:33 +0300

    fix DiaryShareHandler

--- newDiary/src/main/java/com/belka/newDiary/handler/DiaryShareHandler.java
+++ newDiary/src/main/java/com/belka/newDiary/handler/DiaryShareHandler.java
@@ -23,11 +23,13 @@ import java.util.stream.Collectors;
 @Component
 @AllArgsConstructor
 public class DiaryShareHandler extends AbstractBelkaHandler {
-    final static String CODE = "WRITE_DIARY";
+    final static String CODE = "SHARE_DIARY";
     private final static String NEXT_HANDLER = "";
     private final static String PREVIOUS_HANDLER = DiaryWriteHandler.CODE;
-    private final static String PREVIOUS_DATA = DiaryWriteHandler.CODE + DiaryWriteHandler.YES_BUTTON;
-    private final static String ANSWER = "the note has been sent";
+    private final static String PREVIOUS_DATA_YES = DiaryWriteHandler.CODE + DiaryWriteHandler.YES_BUTTON;
+    private final static String PREVIOUS_DATA_NO = DiaryWriteHandler.CODE + DiaryWriteHandler.NO_BUTTON;
+    private final static String ANSWER_FOR_SHARING = "the note has been sent";
+    private final static String ANSWER_FOR_SAVING = "the note has been saved";
     private final static String PREFIX_FOR_NOTE = "note from ";
     private final PreviousService previousService;
     private final DiaryService diaryService;
@@ -37,26 +39,34 @@ public class DiaryShareHandler extends AbstractBelkaHandler {
     @Override
     @Transactional
     public Flux<PartialBotApiMethod<?>> handle(BelkaEvent event) {
-        if (event.isHasCallbackQuery() && event.getData().equals(PREVIOUS_DATA)) {
+        if (event.isHasCallbackQuery() && event.getData().equals(PREVIOUS_DATA_YES)) {
             Long chatId = event.getChatId();
             String note = PREFIX_FOR_NOTE + userService.getName(chatId) + "/n" + diaryService.getNote(LocalDate.now(), chatId);
             Collection<Long> followersId = userService.getFollowersId(chatId);
             Collection<SendMessage> messages = followersId.stream().map(id -> sendMessage(id, note)).collect(Collectors.toList());
 
-            messages.add(sendMessage(chatId, ANSWER));
+            messages.add(sendMessage(chatId, ANSWER_FOR_SHARING));
 
-            previousService.save(PreviousStepDto.builder()
-                    .previousStep(CODE)
-                    .nextStep(NEXT_HANDLER)
-                    .userId(chatId)
-                    .build());
-            statsService.save(StatsDto.builder()
-                    .userId(chatId)
-                    .handlerCode(CODE)
-                    .requestTime(LocalDateTime.now())
-                    .build());
+            savePreviousAndStats(event);
             return Flux.fromIterable(messages);
+        } else if (event.isHasCallbackQuery() && event.getData().equals(PREVIOUS_DATA_NO)) {
+            Long chatId = event.getChatId();
+            savePreviousAndStats(event);
+            return Flux.just(sendMessage(chatId, ANSWER_FOR_SAVING));
         }
         return Flux.empty();
+    }
+
+    private void savePreviousAndStats(BelkaEvent event) {
+        previousService.save(PreviousStepDto.builder()
+                .previousStep(CODE)
+                .nextStep(NEXT_HANDLER)
+                .userId(event.getChatId())
+                .build());
+        statsService.save(StatsDto.builder()
+                .userId(event.getChatId())
+                .handlerCode(CODE)
+                .requestTime(LocalDateTime.now())
+                .build());
     }
 }
