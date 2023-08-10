0755dd4b81f3523c525729360dea3dd22811082f
ushnii <ushniihk@yandex.ru>
2023-08-10 20:52:20 +0300

    fix

--- ApplicationModule/src/main/java/com/belka/BulbaBot/service/TelegramBot.java
+++ ApplicationModule/src/main/java/com/belka/BulbaBot/service/TelegramBot.java
@@ -4,7 +4,6 @@ import com.belka.BulbaBot.config.BotConfig;
 import com.belka.BulbaBot.handler.HandlerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
