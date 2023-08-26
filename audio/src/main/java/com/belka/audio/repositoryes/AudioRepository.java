1b0887c6fdd3887343f0730a8283ce9adef4a847
ushnii <ushniihk@yandex.ru>
2023-08-26 16:54:50 +0300

    add concatenating audio

--- audio/src/main/java/com/belka/audio/repositoryes/AudioRepository.java
+++ audio/src/main/java/com/belka/audio/repositoryes/AudioRepository.java
@@ -5,8 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface AudioRepository extends JpaRepository<AudioEntity, String> {
     @Modifying
     @Query("UPDATE AudioEntity a SET a.isPublic = :flag WHERE a.id = :fileId")
     void changeIsPrivateFlag(boolean flag, String fileId);
+
+    boolean existsByDateAndUserId(LocalDate date, Long userId);
+
+    @Query("select a.id from AudioEntity a WHERE a.date = :date and a.userId = :userId")
+    List<String> getAllIdByDateAndUserId(LocalDate date, Long userId);
 }
--- audio/src/main/java/com/belka/audio/services/AudioServiceImpl.java
+++ audio/src/main/java/com/belka/audio/services/AudioServiceImpl.java
@@ -17,12 +17,15 @@ import org.telegram.telegrambots.meta.api.objects.Voice;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.SequenceInputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -50,6 +53,30 @@ public class AudioServiceImpl implements AudioService {
     @Override
     @Transactional
     public void saveVoice(Voice voice, Long userId) {
+        LocalDate today = LocalDate.now();
+        if (audioRepository.existsByDateAndUserId(today, userId)) {
+            try {
+                List<String> audiosIdInDB = audioRepository.getAllIdByDateAndUserId(today, userId);
+                if (audiosIdInDB.size() > 1) {
+                    throw new RuntimeException("something was wrong and we have two records in one day");
+                }
+                String audioIdInDB = audiosIdInDB.get(0);
+                writeDataToDB(voice, userId);
+                concatenateAudios(
+                        pathToAudio + audioIdInDB + EXTENSION,
+                        pathToAudio + voice.getFileId() + EXTENSION
+                );
+            } finally {
+                deleteVoice(voice.getFileId());
+            }
+        } else {
+            writeDataToDB(voice, userId);
+        }
+
+    }
+
+    @Transactional
+    public void writeDataToDB(Voice voice, Long userId) {
         String fileId = voice.getFileId();
         ResponseEntity<String> response = getFilePath(fileId);
         byte[] downloadFile = downloadFile(getFilePath(response));
@@ -95,9 +122,20 @@ public class AudioServiceImpl implements AudioService {
         return pathToAudio + fileId + EXTENSION;
     }
 
+    @Transactional
+    public void concatenateAudios(String firstAudio, String secondAudio) {
+        Path firstAudioPath = Path.of(firstAudio);
+        try (InputStream inputStream1 = Files.newInputStream(firstAudioPath);
+             InputStream inputStream2 = Files.newInputStream(Path.of(secondAudio));
+             InputStream concatenatedStream = new SequenceInputStream(inputStream1, inputStream2)) {
+            Files.copy(concatenatedStream, firstAudioPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private ResponseEntity<String> getFilePath(String fileId) {
         HttpEntity<String> request = new HttpEntity<>(headers);
-
         return restTemplate.exchange(
                 fileInfoUri,
                 HttpMethod.GET,
